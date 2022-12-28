9cdbcb5e9b751e769c37f4a22548e02adcb91d9d
HarmoniaCodes <harmoniacodes@proton.me>
2022-12-28 08:48:32 -0500

    add initial JS

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// index 0, 1, 2, 3
+const topics = ["HTML", "CSS", "Git", "Javascript"];
+
+// var x = 0;
+// This is the starting point for the loop
+
+// x < topics.length
+// if x is less than the length of the array (condition)
+
+// x++
+// iterate over each item by increasing by 1 each time
+
+for (var i = 0; i < topics.length; i++) {
+  console.log(topics[i]);
+}
+
+// start at index 0. As long as i is less than the length of the array,
+// continue through the array and console.log out each item at each index
